18bc7733f1c2cf14e9dbcffb27cc9c679214ef37
Jasper Wang <jasper.wang@sap.com>
2020-11-20 20:01:26 -0800

    support react

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import ReactDOM from 'react-dom';
+
+function App() {
+    return <div>Hello, World</div>;
+}
+
+export default App;
--- src/index.ts
+++
@@ -1,8 +0,0 @@
-function component(): HTMLElement {
-  const element: HTMLElement = document.createElement('div');
-  element.innerHTML += 'Hello, webpack';
-  return element;
-}
-
-const element: HTMLElement = component();
-document.body.appendChild(element);
