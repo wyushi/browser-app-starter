7a0cd6405691ddae2c9488f1eaf3665566555890
Jasper Wang <jasper.ys.wang@gmail.com>
2020-12-14 00:37:18 -0800

    update configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.tsx',
@@ -19,4 +20,9 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'static/index.html'
+    })
+  ]
 };
